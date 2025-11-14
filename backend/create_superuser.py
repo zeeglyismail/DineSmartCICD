ffeaefcd9769519ccc6c55546f7008c082ebdfaa
zeeglyismail <zeeglyismail@gmail.com>
2025-11-14 11:44:25 +0600

    superuser fix

--- backend/create_superuser.py
+++ backend/create_superuser.py
@@ -4,17 +4,17 @@ from django.db import IntegrityError
 
 User = get_user_model()
 
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+name = os.environ.get("DJANGO_SUPERUSER_NAME", "Admin User")  # optional
 
-if username and password:
+if email and password:
     try:
-        if not User.objects.filter(username=username).exists():
+        if not User.objects.filter(email=email).exists():
             User.objects.create_superuser(
-                username=username,
                 email=email,
-                password=password
+                password=password,
+                name=name
             )
             print("Superuser created successfully.")
         else:
